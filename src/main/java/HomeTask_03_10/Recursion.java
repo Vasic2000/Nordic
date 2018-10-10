03b6a6e042cb454840dcfbaeceffa9bf2f869fd6
vasic2000 <vasic2000@yandex.ru>
2018-10-10 12:20:57 +0300

    Ost

--- src/main/java/HomeTask_03_10/Recursion.java
+++ src/main/java/HomeTask_03_10/Recursion.java
@@ -5,13 +5,30 @@ import javax.swing.JOptionPane;
 public class Recursion {
     public static void main(String[] args) {
         int N = Integer.parseInt(JOptionPane.showInputDialog(null, "До которой цифры набирать строку?", JOptionPane.PLAIN_MESSAGE));
-        for (int i = 1; i <= N; i++)
-            System.out.println((Luzz(1, N)));
+
+//        Неправильно, через конкатенацию
+        String s = "";
+        for(int i = 1; i <= N; i++) {
+            s = s + i;
+        }
+        System.out.println(s);
+
+//        Правильно, через StringBuilding
+        StringBuilder str = new StringBuilder();
+        for(int i = 1; i <= N; i++) {
+            str.append(i);
+        }
+        System.out.println(str);
+
+//        Через рекурчию
+        System.out.println((Luzz(1, N)));
     }
 
     public static StringBuilder Luzz(int i, int j) {
         StringBuilder result = new StringBuilder();
-
+        if(i <= j) {
+            result.append(i + "" + Luzz(i + 1, j));
+        }
         return result;
     }
 }
